ddf11ef5f8455001c71e2ad412510ff161d95f89
aliuql <aliuql@qq.com>
2024-08-10 06:37:50 +0800

    test

--- grpc-demo-svc/src/main/java/com/grpc/demo/GrpcServer.java
+++ grpc-demo-svc/src/main/java/com/grpc/demo/GrpcServer.java
@@ -4,9 +4,11 @@ import com.grpc.demo.service.HelloServiceImpl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 public class GrpcServer {
     public static void main(String[] args) throws Exception {
-        Server server = ServerBuilder.forPort(9092)
+        Server server = ServerBuilder.forPort(9092).handshakeTimeout(86400, TimeUnit.SECONDS)
                 .addService(new HelloServiceImpl())
                 .build();
         server.start();
